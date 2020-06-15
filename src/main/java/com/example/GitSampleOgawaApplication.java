517466998ad56237516d59f0248361718079b2d1
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-06-15 13:56:44 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/GitSampleOgawaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleOgawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleOgawaApplication.class, args);
+	}
+
+}
